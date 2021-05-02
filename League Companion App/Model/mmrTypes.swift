09a1806db6ca95f58134f205eba2a7a34edfafa6
Clayton Watkins <watkinclayton17@yahoo.com>
2021-05-02 09:13:34 -0700

    Added project files

---
+++ League Companion App/Model/mmrTypes.swift
@@ -0,0 +1,8 @@
+//
+//  mmrTypes.swift
+//  League Companion App
+//
+//  Created by Clayton Watkins on 5/2/21.
+//
+
+import Foundation
---
+++ League Companion App/ModelController/NetworkController.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkController.swift
+//  League Companion App
+//
+//  Created by Clayton Watkins on 5/2/21.
+//
+
+import Foundation
