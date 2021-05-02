9394bec9c18d4d89c12336f1e757c42f7f504565
Clayton Watkins <watkinclayton17@yahoo.com>
2021-05-02 09:38:57 -0700

    Wrote first network request to test getting data with a summoner name from NA only

--- League Companion App/ModelController/NetworkController.swift
+++ League Companion App/ModelController/NetworkController.swift
@@ -15,9 +15,36 @@ enum NetworkError: Error {
 
 class NetworkController {
     static let shared = NetworkController()
-    let baseURL = URL(string: "https://na.whatismymmr.com/api/v1/summoner?name=")
+    let baseURL = URL(string: "https://na.whatismymmr.com/api/v1/summoner?name=")!
     
     func getMMR(summonerName: String, completion: @escaping (Result<mmr, NetworkError>) -> Void) {
+        let requestURL = baseURL.appendingPathComponent(summonerName)
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = "GET"
         
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                print(error)
+                completion(.failure(.tryAgain))
+                return
+            }
+            
+            if let response = response {
+                print(response)
+            }
+            
+            guard let data = data else {
+                completion(.failure(.noData))
+                return
+            }
+            
+            do {
+                let returnedMMRdata = try JSONDecoder().decode(MMR.self, from: data)
+                completion(.success(returnedMMRdata))
+            } catch {
+                print("Error decoding MMR data: \(error)")
+            }
+        }
+        task.resume()
     }
 }
