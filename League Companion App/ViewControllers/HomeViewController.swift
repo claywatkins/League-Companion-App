9463283d0a1ad0218058aad608cf02e40e76aeb9
Clayton Watkins <watkinclayton17@yahoo.com>
2021-05-02 10:26:11 -0700

    Added IBOutlets and IBActions. Added function when tapping search button to fetch summoner MMR from API

--- League Companion App/ViewControllers/HomeViewController.swift
+++ League Companion App/ViewControllers/HomeViewController.swift
@@ -8,22 +8,42 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    // MARK: - IBOutlet
+    @IBOutlet weak var rankedMMRLabel: UILabel!
+    @IBOutlet weak var normalMMRLabel: UILabel!
+    @IBOutlet weak var summonerNameTextField: UITextField!
+    
+    // MARK: - Properties
+    var networkController = NetworkController.shared
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - IBAction
+    @IBAction func searchButtonTapped(_ sender: Any) {
+        guard let summonerName = summonerNameTextField.text, !summonerName.isEmpty else { return }
+        networkController.getMMR(summonerName: summonerName) { result in
+            do {
+                let returnedMMR = try result.get()
+                DispatchQueue.main.async {
+                    if let rankedMMR = returnedMMR.ranked.avg {
+                        self.rankedMMRLabel.text = "\(rankedMMR)"
+                    } else {
+                        self.rankedMMRLabel.text = "No MMR Found"
+                    }
+                    if let normalMMR = returnedMMR.normal.avg {
+                        self.normalMMRLabel.text = "\(normalMMR)"
+                    } else {
+                        self.rankedMMRLabel.text = "No MMR Found"
+                    }
+                }
+            } catch {
+                print("Error getting returned data: \(error)")
+            }
+        }
     }
-    */
+    
 
 }
