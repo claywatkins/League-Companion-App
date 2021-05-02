43242feb56606a49d23f3bda32433e7abe68a02c
Clayton Watkins <watkinclayton17@yahoo.com>
2021-05-02 14:43:53 -0700

    Created a dataController to host any held data

---
+++ League Companion App/ModelController/DataController.swift
@@ -0,0 +1,15 @@
+//
+//  DataController.swift
+//  League Companion App
+//
+//  Created by Clayton Watkins on 5/2/21.
+//
+
+import Foundation
+
+class DataController {
+    // MARK: - Properties
+    static let shared = DataController()
+    var rankedHistory: [Historical]? = []
+    var normalHistory: [Historical]? = []
+}
