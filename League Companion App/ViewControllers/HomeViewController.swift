66ce7d449c9327bc77d9508df169bba9c71e8d36
Clayton Watkins <watkinclayton17@yahoo.com>
2021-05-02 12:14:29 -0700

    When getting data from API, activity indicator is shown to indicate loading

--- League Companion App/ViewControllers/HomeViewController.swift
+++ League Companion App/ViewControllers/HomeViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 class HomeViewController: UIViewController {
     // MARK: - IBOutlet
     @IBOutlet weak var rankedMMRLabel: UILabel!
+    @IBOutlet weak var rankedMMRActivityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var normalMMRLabel: UILabel!
+    @IBOutlet weak var normalMMRActivityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var summonerNameTextField: UITextField!
     
     // MARK: - Properties
@@ -19,30 +21,45 @@ class HomeViewController: UIViewController {
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        hideUIElements()
+    }
+    
+    // MARK: - Methods
+    private func hideUIElements() {
         rankedMMRLabel.isHidden = true
+        rankedMMRActivityIndicator.isHidden = true
         normalMMRLabel.isHidden = true
+        normalMMRActivityIndicator.isHidden = true
     }
     
     // MARK: - IBAction
     @IBAction func searchButtonTapped(_ sender: Any) {
         guard let summonerName = summonerNameTextField.text, !summonerName.isEmpty else { return }
-        networkController.getMMR(summonerName: summonerName) { result in
+        rankedMMRActivityIndicator.isHidden = false
+        rankedMMRActivityIndicator.startAnimating()
+        normalMMRActivityIndicator.isHidden = false
+        normalMMRActivityIndicator.startAnimating()
+        networkController.getMMR(summonerName: summonerName) { [weak self] result in
             do {
                 let returnedMMR = try result.get()
                 DispatchQueue.main.async {
+                    self?.rankedMMRActivityIndicator.stopAnimating()
+                    self?.rankedMMRActivityIndicator.isHidden = true
+                    self?.normalMMRActivityIndicator.stopAnimating()
+                    self?.normalMMRActivityIndicator.isHidden = true
                     if let rankedMMR = returnedMMR.ranked.avg {
-                        self.rankedMMRLabel.isHidden = false
-                        self.rankedMMRLabel.text = "\(rankedMMR)"
+                        self?.rankedMMRLabel.isHidden = false
+                        self?.rankedMMRLabel.text = "\(rankedMMR)"
                     } else {
-                        self.rankedMMRLabel.isHidden = false
-                        self.rankedMMRLabel.text = "No MMR Found"
+                        self?.rankedMMRLabel.isHidden = false
+                        self?.rankedMMRLabel.text = "No MMR Found"
                     }
                     if let normalMMR = returnedMMR.normal.avg {
-                        self.normalMMRLabel.isHidden = false
-                        self.normalMMRLabel.text = "\(normalMMR)"
+                        self?.normalMMRLabel.isHidden = false
+                        self?.normalMMRLabel.text = "\(normalMMR)"
                     } else {
-                        self.normalMMRLabel.isHidden = false
-                        self.rankedMMRLabel.text = "No MMR Found"
+                        self?.normalMMRLabel.isHidden = false
+                        self?.rankedMMRLabel.text = "No MMR Found"
                     }
                 }
             } catch {
