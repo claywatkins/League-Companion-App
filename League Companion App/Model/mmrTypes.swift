c31427344af8600bccbcb872d012b3a90d679aec
Clayton Watkins <watkinclayton17@yahoo.com>
2021-05-02 09:39:11 -0700

    changed capitalization on structs

--- League Companion App/Model/mmrTypes.swift
+++ League Companion App/Model/mmrTypes.swift
@@ -7,17 +7,17 @@
 
 import Foundation
 
-struct mmr: Codable {
+struct MMR: Codable {
     var ranked: ranked
     var normal: normal
 }
 
-struct ranked: Codable {
+struct Ranked: Codable {
     var avg: Int?
     var timestamp: Int?
 }
  
-struct normal: Codable {
+struct Normal: Codable {
     var avg: Int?
     var timestamp: Int?
 }
